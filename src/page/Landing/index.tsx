c0d54ab8f3c3734b441b733a24ce249e6d28e8c4
hanifaafauziah <hanifa260402@gmail.com>
2022-06-23 17:08:07 +0700

    header footer login register page yey

---
+++ src/page/Landing/index.tsx
@@ -0,0 +1,12 @@
+
+import Header from '../../component/header'
+import Footer from '../../component/footer'
+
+export default function Landing() {
+    return (
+    <div>
+    <Header/>
+    <Footer/>
+    </div>
+    );
+  }
